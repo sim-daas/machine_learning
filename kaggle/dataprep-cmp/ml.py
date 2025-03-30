b341c098356e49f21b0b8d4e809a9aa17ffbba8e
ubuntu <aitechmanml@gmail.com>
2025-03-30 22:57:52 +0530

    hello

--- kaggle/dataprep-cmp/ml.py
+++ kaggle/dataprep-cmp/ml.py
@@ -16,20 +16,29 @@ df.sample(5)
 
 df.info()
 df.describe()
-df.corr(numeric_only=)
+corr = df.corr(numeric_only=True)
+df.dtypes
 
 
+df['date'] = pd.to_datetime(df['date'], dayfirst=True)
+df['hour'] = df['date'].dt.hour
+df['min'] = df['date'].dt.minute
+df.drop(columns=['date', 'rv1', 'rv2', 'Visibility', 'Tdewpoint'])
 
 
+plt.figure(figsize=(22, 12))
+sns.heatmap(corr, annot=True, cmap='coolwarm', linewidths=0.5)
+plt.title('Correlation Heatmap')
+plt.show()
+
+df.loc[:, ['Appliances', 'lights','T2', 'T3','RH_5','RH_out','hour']].describe()
 
 
+sns.displot(data=df, x='lights', kde=True )
+plt.title('light distplot')
+plt.show()
 
-
-
-
-
-
-
+from sklearn.preprocessing import MinMaxScaler
 
 
 
