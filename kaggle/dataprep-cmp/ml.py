186751d0c8ba0cddc518dcaa2d809f71e4eb3c36
ubuntu <aitechmanml@gmail.com>
2025-03-30 19:05:05 +0530

    hello

--- kaggle/dataprep-cmp/ml.py
+++ kaggle/dataprep-cmp/ml.py
@@ -2,11 +2,16 @@ import numpy as np
 import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
-import 
+import sklearn
+import warnings
 
 
+pd.set_option("display.max_columns", None)
+sklearn.set_config(transform_output="pandas")
+warnings.filterwarnings("ignore")
 
 
+df = pd.read_csv('')
 
 
 
