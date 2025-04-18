a52c1515cb792ee0b3b794654e9f600b3e182442
ubuntu <aitechmanml@gmail.com>
2025-04-18 09:42:36 +0530

    hello

--- kaggle/car-becho/first.py
+++ kaggle/car-becho/first.py
@@ -2,5 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import skimpy as skim
 
-df 
+df = pd.read_csv('train.csv', index_col=0)
+df.sample(5)
+df.info()
+
+df.columns
+
+skim.skim(df)
+
+use_cols = df.columns.to_list()
+
+df.isna().sum()
+
+df[df.fuel_type.isna() & df.engine.str.contains('Electric')]
+
+for col in df.columns:
+    if df[col].dtype == 'object':
+        df[col] = df[col].str.strip()
+        print(df[col].nunique())
+
+df.drop_duplicates(inplace=True)
+
+df.drop()
+
+
+
+
+
+
+
+
+
+
+
+ 
